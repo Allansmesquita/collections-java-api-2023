a3f8e0c9d5c11a7ba38f90159280f3eb285c0587
Allan Mesquita <allan_santoz@hotmail.com>
2023-10-03 14:40:25 -0300

    Resolvendo exercicios List

--- .idea/sonarlint/securityhotspotstore/index.pb
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -5,3 +5,13 @@ d
 4src/main/java/list/OperacoesBasicas/ListaTarefa.java,3\c\3cb7f4e352535a3f46445df78cc9b604bbf92206
 _
 /src/main/java/list/OperacoesBasicas/Tarefa.java,1\f\1f288f28b04530570693fae3c2fd3337883bed45
+V
+&src/main/java/list/Pesquisa/Livro.java,1\f\1f5be24dd6853c67a0abffbab29411a6813b3e43
+_
+/src/main/java/list/Pesquisa/CatalogoLivros.java,8\1\81eaed0acdb094edb7a7c089936b07e62bcf0f23
+X
+(src/main/java/list/Ordenacao/Pessoa.java,d\8\d89689b935bd111aeedbca8eed6d8fde5e04f0e9
+e
+5src/main/java/list/Ordenacao/ComparatorPorAltura.java,d\e\de07d1a7d00359496ed6507b414c8806e8368af6
+a
+1src/main/java/list/Ordenacao/OrdenacaoPessoa.java,b\f\bf344bb6f65353cf36a9cf475a4778c3f4f42dbe
---
+++ src/main/java/list/Ordenacao/OrdenacaoPessoa.java
@@ -0,0 +1,40 @@
+package main.java.list.Ordenacao;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class OrdenacaoPessoa {
+    //atributo
+    private List<Pessoa> pessoaList;
+
+    public OrdenacaoPessoa() {
+        this.pessoaList = new ArrayList<>();
+    }
+    public void adicionarPessoa(String nome, int idade, double altura){
+        pessoaList.add(new Pessoa(nome, idade, altura));
+    }
+
+    public List<Pessoa> ordenarPorIdade() {
+        List<Pessoa> pessoasPorIdade = new ArrayList<>(pessoaList);
+        Collections.sort(pessoasPorIdade); //o metodo sort vai entender que tem que ordenar a lista atraves do comparable
+                                           //(pois a classe pessoa "implements comparable<Pessoa>
+        return pessoasPorIdade;
+    }
+    public List<Pessoa> ordenarPorAltura(){
+        List<Pessoa> pessoasPorAltura = new ArrayList<>(pessoaList);
+        Collections.sort(pessoasPorAltura,new ComparatorPorAltura()); // quando eu não passo o comparator, o collections vai ordenar usando o comparable
+        return pessoasPorAltura;
+    }
+
+    public static void main(String[] args) {
+        OrdenacaoPessoa ordenacaoPessoa = new OrdenacaoPessoa();
+        ordenacaoPessoa.adicionarPessoa("Nome 1",20,1.56);
+        ordenacaoPessoa.adicionarPessoa("Nome 2",30,1.80);
+        ordenacaoPessoa.adicionarPessoa("Nome 3",25,1.78);
+        ordenacaoPessoa.adicionarPessoa("Nome 4",17,1.56);
+
+        System.out.println(ordenacaoPessoa.ordenarPorIdade());
+        System.out.println(ordenacaoPessoa.ordenarPorAltura());
+    }
+}
